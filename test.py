ebabf2a125e873d0e5cf70460b8e6e245fc5d87b
Armin Samii <armin@avametric.com>
2016-07-09 15:20:42 -0700

    Attempt to animate but quality is awful

--- test.py
+++ test.py
@@ -1,14 +1,26 @@
 import gpxpy 
 import gpxpy.gpx
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import tilestitcher
 
 def gpxVisitor(gpx):
     """ Visit each point in the strava GPX file. """
-    for track in gpx.tracks: 
-        for segment in track.segments: 
-            for point in segment.points: 
-                yield point
+    if hasattr(gpx, 'tracks'):
+        for track in gpx.tracks:
+            for segment in track.segments:
+                for point in segment.points:
+                    yield point
+    else:
+        for point in gpx:
+            yield point
+
+def getSegment(gpx):
+    """ Assume there is only one segment, and return it. """
+    assert len(gpx.tracks) == 1
+    track = gpx.tracks[0]
+    assert len(track.segments) == 1
+    return track.segments[0]
 
 def latitudeVisitor(gpx):
     for point in gpxVisitor(gpx):
@@ -26,13 +38,12 @@ def getImagePt(slipy_map, image_info, lat, lon):
     return slipy_map._get_position_on_stitched_image(
         image_info.tile_1, image_info.tile_2, lat, lon)
 
-def visualizeGpxOnMap(slipy_map, gpx, mapImage, image_info):
+def visualizeGpxOnMap(fig, slipy_map, gpx, mapImage, image_info):
     """ Draw a gpx track on the given mapImage.  """
     pts = [getImagePt(slipy_map, image_info, pt.latitude, pt.longitude)
             for pt in gpxVisitor(gpx)]
 
-    # Create figure
-    fig = plt.figure(facecolor = '0.05')
+    fig.clear()
 
     # Remove axes
     ax = plt.Axes(fig, [0., 0., 1., 1.])
@@ -50,13 +61,46 @@ def visualizeGpxOnMap(slipy_map, gpx, mapImage, image_info):
     plt.imshow(mapImage, origin='upper', aspect='normal')
 
     # Visualization
-    plt.show()
-    # plt.savefig('plot.png', dpi = 80)
+    #plt.show()
+    #plt.savefig('plot.png', dpi = 80)
+
+    return ax,
 
 def visualizeGpx(gpx):
     slipy_map = tilestitcher.SlippyMapTiles(max_tiles=10)
     (mapImage, imageInfo) = getMapImageAndImageInfoForGpx(slipy_map, gpx)
     visualizeGpxOnMap(slipy_map, gpx, mapImage, imageInfo)
+
+def getPointsFrom(gpxSegment, startPoint, numFrames):
+    count = 0
+    for pt in gpxSegment.walk(startPoint):
+        if count == numFrames: return
+        yield pt
+        count = count+1
+
+    # If this is hit, numFrames exceeds total number of frames
+    assert False
+
+def getPointsBetween(gpxSegment, startPoint, endPoint):
+    for pt in gpxSegment.walk(startPoint):
+        if pt == endPoint: return
+        yield pt
+
+    # If this is hit, endPoint is not a point after startPoint
+    assert False
+
+def visualizeGpxAtTime(numFrames, fig, gpx, timeStart, slipy_map, mapImage, image_info):
+    print numFrames
+    segment = getSegment(gpx)
+    startPoint = segment.get_location_at(timeStart)
+    subGpx = [x for x in getPointsFrom(segment, startPoint, 1000+numFrames*100)]
+    return visualizeGpxOnMap(fig, slipy_map, subGpx, mapImage, image_info)
+
+def animateGpxAtTime(gpx, timeStart, timeEnd, slipy_map, mapImage, image_info):
+    fig = plt.figure(facecolor = '0.05')
+    line_ani = animation.FuncAnimation(fig, visualizeGpxAtTime, frames=100,
+        fargs=(fig, gpx, timeStart, slipy_map, mapImage, image_info),
+        interval=50, blit=False)
 
 if __name__ is "main":
     gpx_file = open('test.gpx', 'r') 
